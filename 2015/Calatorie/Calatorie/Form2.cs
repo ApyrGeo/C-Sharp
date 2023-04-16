2f03a309436fd8b98354851d45f220009bd01784
ApyrGeo <91793381+ApyrGeo@users.noreply.github.com>
2023-04-16 16:31:29 +0300

    Add files via upload

---
+++ 2015/Calatorie/Calatorie/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calatorie
+{
+    public partial class Form2 : Form
+    {
+        private int I;
+        public Form2(int i)
+        {
+            InitializeComponent();
+            I = i;
+            if(i==1)
+            {
+                menuStrip1.Items.Add("Administrare");
+                menuStrip1.Items.Add("Iesire");
+            }
+            if(i==2)
+            {
+                menuStrip1.Items.Add("Turisti");
+                menuStrip1.Items.Add("Iesire");
+            }
+        }
+        public Form5 frm5 = new Form5();
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem == menuStrip1.Items[1])
+            {
+                Application.Exit();
+            }
+            if (e.ClickedItem == menuStrip1.Items[0])
+            {
+                if (I == 1)
+                {
+                    Form3 frm3 = new Form3();
+                    frm5.MdiParent = this;
+                    frm3.MdiParent = this;
+                    frm3.Show();
+                }
+                if(I==2)
+                {
+                    Form4 frm4 = new Form4();
+                    frm4.MdiParent = this;
+                    frm4.Show();
+                }
+            }
+        }
+        public void close()
+        {
+            this.Close();
+        }
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ 2015/Calatorie/Calatorie/Form4.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calatorie
+{
+    public partial class Form4 : Form
+    {
+        private SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DBTimpSpatiu.mdf;Integrated Security=True;Connect Timeout=30");
+        private string[] porturi = new string[] { "Constanta", "Varna", "Burgas", "Istanbul", "Kozlu", "Samsun", "Batumi", "Sokhumi", "Sochi", "Anapa", "Ialta", "Sevastopol", "Odessa" };
+
+        public Form4()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = comboBox1.SelectedIndex;
+            int tip = 3;
+            dataGridView1.Rows.Clear();
+            if (i == 0)
+            {
+                tip = 3;
+            }
+            if (i == 1)
+            {
+                tip = 5;
+            }
+            if (i == 2)
+            {
+                tip = 8;
+            }
+
+            con.Open();
+            SqlCommand select = new SqlCommand($"SELECT * FROM Croaziere WHERE Tip_Croaziera={tip}", con);
+            SqlDataReader read = select.ExecuteReader();
+            while (read.Read())
+            {
+                string[] traseu = read.GetString(2).Split(' ');
+                string final = "";
+                for (int j = 0; j < traseu.Length; j++)
+                {
+                    final += porturi[Convert.ToInt32(traseu[j]) - 1] + ", ";
+                }
+                dataGridView1.Rows.Add(
+                    read.GetInt32(1),
+                    read.GetInt32(0),
+                    final,
+                    read.IsDBNull(3) ? null : read.GetDateTime(3).ToString(),
+                    read.IsDBNull(4) ? null : read.GetDateTime(4).ToString(),
+                    read.GetInt32(5),
+                    read.IsDBNull(6) ? null : read.GetInt32(4).ToString());
+
+
+            }
+            con.Close();
+        }
+        private int selectie=-1;
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i=e.RowIndex;
+            selectie = (int)dataGridView1[1, i].Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(selectie!=-1)
+            {
+                Form6 frm6=new Form6(selectie);
+                this.Hide();
+                frm6.MdiParent = this.MdiParent;
+                frm6.Show();
+                this.Close();
+            }
+        }
+    }
+}
