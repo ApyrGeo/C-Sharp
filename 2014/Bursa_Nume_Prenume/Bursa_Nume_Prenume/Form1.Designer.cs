c4610381904636fe3f674709630550bff2c544f2
ApyrGeo <91793381+ApyrGeo@users.noreply.github.com>
2023-04-21 19:06:11 +0300

    Add files via upload

---
+++ 2014/Bursa_Nume_Prenume/Bursa_Nume_Prenume/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Bursa_Nume_Prenume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.utilizatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actiunileMeleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graficProfitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.utilizatorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(904, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // utilizatorToolStripMenuItem
+            // 
+            this.utilizatorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actiunileMeleToolStripMenuItem,
+            this.graficProfitToolStripMenuItem});
+            this.utilizatorToolStripMenuItem.Name = "utilizatorToolStripMenuItem";
+            this.utilizatorToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.utilizatorToolStripMenuItem.Text = "Utilizator";
+            // 
+            // actiunileMeleToolStripMenuItem
+            // 
+            this.actiunileMeleToolStripMenuItem.Name = "actiunileMeleToolStripMenuItem";
+            this.actiunileMeleToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.actiunileMeleToolStripMenuItem.Text = "Actiunile mele";
+            this.actiunileMeleToolStripMenuItem.Click += new System.EventHandler(this.actiunileMeleToolStripMenuItem_Click);
+            // 
+            // graficProfitToolStripMenuItem
+            // 
+            this.graficProfitToolStripMenuItem.Name = "graficProfitToolStripMenuItem";
+            this.graficProfitToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.graficProfitToolStripMenuItem.Text = "Grafic profit";
+            this.graficProfitToolStripMenuItem.Click += new System.EventHandler(this.graficProfitToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(904, 85);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(401, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rata reimprospatare";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(562, 28);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(119, 26);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(207, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Inchide bursa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(27, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Deschide bursa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column9,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.dataGridView1.Location = new System.Drawing.Point(65, 336);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Size = new System.Drawing.Size(724, 157);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Denumire";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Numar actiuni";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Valoare actiune INITIAL";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Valoare actiune momentan";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Valoare cu care a crescut actiunea momentan";
+            this.Column5.Name = "Column5";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Total valoare initiala";
+            this.Column9.Name = "Column9";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Total valoare momentana";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Profit / Pierdere momentana";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Profit / Pierdere total";
+            this.Column8.Name = "Column8";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(352, 256);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(200, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 539);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BURSA";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem utilizatorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actiunileMeleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem graficProfitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 2014/Bursa_Nume_Prenume/Bursa_Nume_Prenume/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+
+namespace Bursa_Nume_Prenume
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DBBursa.mdf;Integrated Security=True;Connect Timeout=30");
+
+        public Form1()
+        {
+            InitializeComponent();
+            con.Open();
+
+            new SqlCommand("TRUNCATE TABLE Actiuni;INSERT INTO Actiuni VALUES ('Azomed',5,25);\r\nINSERT INTO Actiuni VALUES ('Tepra',15,12);\r\nINSERT INTO Actiuni VALUES ('Raddin',0,4);\r\nINSERT INTO Actiuni VALUES ('Nelacom',200,8);\r\nINSERT INTO Actiuni VALUES ('Daleprod',0,7);", con).ExecuteNonQuery();
+            con.Close();
+
+
+           
+            con.Open();
+            SqlCommand select = new SqlCommand("SELECT * FROM Actiuni", con);
+            SqlDataReader read = select.ExecuteReader();
+            while (read.Read())
+            {
+                dataGridView1.Rows.Add(
+                    read.GetString(1),
+                    read.GetInt32(2).ToString(),
+                    read.GetInt32(3).ToString(),
+                    read.GetInt32(3).ToString(),
+                    "",
+                    (read.GetInt32(2) * read.GetInt32(3)).ToString(),
+                    "",
+                    "",
+                    ""
+                    );
+            }
+
+            con.Close();
+            if (on==true)
+            {
+                
+            }
+        }
+        private void actiunileMeleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Form2 frm2 = new Form2(timer1.Interval,on);
+            frm2.Show();
+        }
+        private bool on = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button1.Enabled = false;
+            on = true;
+            
+
+            timer1.Start();
+            int y = 0;
+            timer1.Tick += (a, b) =>
+            {
+                y++;
+                int s = 0;
+                Globals.bursa.Add(new(int, int, int, int, int)[5]);
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    Random x = new Random();
+                    int xx = x.Next(-5, 6);
+
+
+                    
+
+                    dataGridView1[4, i].Value = xx.ToString();
+                    dataGridView1[3, i].Value = (Convert.ToInt32(dataGridView1[3, i].Value) + xx).ToString();
+                    dataGridView1[6, i].Value = (Convert.ToInt32(dataGridView1[1, i].Value) * Convert.ToInt32(dataGridView1[3, i].Value)).ToString();
+                    dataGridView1[7, i].Value = (Convert.ToInt32(dataGridView1[1, i].Value) * Convert.ToInt32(dataGridView1[4, i].Value)).ToString();
+                    dataGridView1[8, i].Value = (Convert.ToInt32(dataGridView1[6, i].Value) - Convert.ToInt32(dataGridView1[5, i].Value)).ToString();
+                    s += Convert.ToInt32(dataGridView1[8, i].Value);
+                    Globals.bursa.Last()[i] = (
+                    Convert.ToInt32(dataGridView1[3, i].Value) + xx,
+                    xx,
+                    Convert.ToInt32(dataGridView1[1, i].Value) * Convert.ToInt32(dataGridView1[3, i].Value),
+                    Convert.ToInt32(dataGridView1[1, i].Value) * Convert.ToInt32(dataGridView1[4, i].Value),
+                    Convert.ToInt32(dataGridView1[6, i].Value) - Convert.ToInt32(dataGridView1[5, i].Value)
+                    );
+                }
+                Globals.suma.Add(s);
+                textBox1.Text = s.ToString();
+
+                dataGridView1.Refresh();
+
+            };
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            while (Application.OpenForms["Form3"] != null)
+            {
+                Application.OpenForms["Form3"].Close();
+            }
+            while (Application.OpenForms["Form2"] != null)
+            {
+                Application.OpenForms["Form2"].Close();
+            }
+            on = false;
+            timer1.Stop();
+            StreamWriter write = new StreamWriter(@"rezultate.txt");
+            write.Write(Globals.suma.Last());
+            write.Close();
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)numericUpDown1.Value;
+        }
+
+        private void graficProfitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 frm3 = new Form3(timer1.Interval, on);
+            frm3.Show();
+        }
+    }
+}
